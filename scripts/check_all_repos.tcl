42c92873ca2ae8b10067560a094594ab0ba0c2d2
Yuri Panchul <yuri@panchul.com>
2023-06-25 00:30:10 -0700

    Cosmetic change

--- scripts/check_all_repos.tcl
+++ scripts/check_all_repos.tcl
@@ -17,18 +17,18 @@ proc check_git_status {dir} {
   if {! [file isdirectory ".git"]} { return }
   my_info $dir
 
-  set files [exec git diff --name-status --diff-filter=R HEAD]
+  set ret [exec git diff --name-status --diff-filter=R HEAD]
 
-  if {$files ne ""} {
-    my_error \n$files \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \nThere are renamed files in the tree. \
       \nYou should check them in before preparing a release package.
   }
 
-  set files [exec git ls-files --others --exclude-standard]
+  set ret [exec git ls-files --others --exclude-standard]
 
-  if {$files ne ""} {
-    my_error \n$files \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \nThere are untracked files in the tree. \
       \nYou should either remove or check them in \
       before preparing a release package. \
@@ -42,36 +42,36 @@ proc check_git_status {dir} {
       the files from the .gitignore list.
   }
 
-  set files [exec git ls-files --others]
+  set ret [exec git ls-files --others]
 
-  if {$files ne ""} {
-    my_error \n$files \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \nThere are files in the tree, ignored by git, \
       based on .gitignore list. \
       \nThis repository is not supposed to have the ignored files. \
       \nYou need to remove them before preparing a release package.
   }
 
-  set files [exec git ls-files --modified]
+  set ret [exec git ls-files --modified]
 
-  if {$files ne ""} {
-    my_error \n$files \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \nThere are modified files in the tree. \
       \nYou should check them in before preparing a release package.
   }
 
-  set not_pushed [exec git cherry -v]
+  set ret [exec git cherry -v]
 
-  if {$not_pushed ne ""} {
-    my_error \n$not_pushed \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \n\nThere are commits which are not pushed (checked using \"git cherry\").
       \nYou should run \"git push\" before preparing a release package.
   }
 
-  set not_pushed [exec git log --branches --not --remotes]
+  set ret [exec git log --branches --not --remotes]
   
-  if {$not_pushed ne ""} {
-    my_error \n$not_pushed \
+  if {$ret ne ""} {
+    my_error \n$ret \
       \n\nThere are commits which are not pushed (checked using \"git log\").
       \nYou should run \"git push\" before preparing a release package.
   }
