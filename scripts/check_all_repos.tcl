2d22d985db0fbbcdfbd20455c846e50ac18bbdaf
Yuri Panchul <yuri@panchul.com>
2023-06-24 23:46:00 -0700

    Fixed a tcl script

--- scripts/check_all_repos.tcl
+++ scripts/check_all_repos.tcl
@@ -113,8 +113,7 @@ foreach repo_path $repo_paths {
 if {$argc == 1 && [lindex $argv 0] == "-pull"} {
   foreach repo_path $repo_paths {
     cd $repo_path
-    set pulled [exec git pull]
-    puts $pulled
+    exec git pull
   }
 } elseif {$argc != 0} {
   my_info "Usage: $script \[-pull\]"
