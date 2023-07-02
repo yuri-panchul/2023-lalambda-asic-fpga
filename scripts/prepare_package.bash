0c7efed1b9430a5430243d57bae130f58554b6af
Yuri Panchul <yuri@panchul.com>
2023-07-02 08:02:55 -0700

    Comsmetic changes

--- scripts/prepare_package.bash
+++ scripts/prepare_package.bash
@@ -114,28 +114,33 @@ else
 
     if [ -n "${f-}" ]
     then
-        error "there are untracked files in the tree."          \
-              "\nYou should either remove or check them in"     \
-              "before preparing a release package."             \
-              "\nSpecifically:\n\n$f"                           \
-              "\n\nYou can also see the file list by running:"  \
-              "\n    git clean -d -n \"$repo_dir\""             \
-              "\n\nAfter reviewing (be careful!),"              \
-              "you can remove them by running:"                 \
-              "\n    git clean -d -f \"$repo_dir\""             \
-              "\n\nNote that \"git clean\" does not see"        \
-              "the files from the .gitignore list."
+        error "there are untracked files in the tree."             \
+              "\nYou should either remove or check them in"        \
+              "before preparing a release package."                \
+              "\nSpecifically:\n\n$f"                              \
+              "\n\nYou can also see the file list by running:"     \
+              "\n    git clean -d -n \"$repo_dir\""                \
+              "\n\nAfter reviewing (be careful!),"                 \
+              "you can remove them by running:"                    \
+              "\n    git clean -d -f \"$repo_dir\""                \
+              "\n\nNote that \"git clean\" without \"-x\" option"  \
+              "does not see the files from the .gitignore list."
     fi
 
     f=$(git ls-files --others)
 
     if [ -n "${f-}" ]
     then
-        error "there are files in the tree, ignored by git,"                    \
-              "based on .gitignore list."                                       \
-              "\nThis repository is not supposed to have the ignored files."    \
-              "\nYou need to remove them before preparing a release package."   \
-              "\nSpecifically:\n\n$f"
+        error "there are files in the tree, ignored by git,"                   \
+              "based on .gitignore list."                                      \
+              "\nThis repository is not supposed to have the ignored files."   \
+              "\nYou need to remove them before preparing a release package."  \
+              "\nSpecifically:\n\n$f"                                          \
+              "\n\nYou can also see the file list by running:"                 \
+              "\n    git clean -d -x -n \"$repo_dir\""                         \
+              "\n\nAfter reviewing (be careful!),"                             \
+              "you can remove them by running:"                                \
+              "\n    git clean -d -x -f \"$repo_dir\""
     fi
 
     f=$(git ls-files --modified)
